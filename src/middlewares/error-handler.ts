ea3df22e0ebac55742301ac35b057677149f5ef6
Arifur rahman <arifur.sew@gmail.com>
2026-09-12 12:24:48 +0600

    feat: add centralized error handling

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,20 @@
+import type { Request, Response, NextFunction } from "express";
+
+import { AppError } from "../core/errors/app-error.js";
+
+export const globalErrorHandler = (error: unknown, req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.log(error);
+
+  if (error instanceof AppError) {
+    return res.status(error.statusCode)
+      .json({ success: false, message: error.message });
+  }
+
+  return res.status(500).json({
+    success: false,
+    message: "Internal server error!",
+  });
+};
