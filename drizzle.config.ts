8c634bf8a68f0564e9269930dd3c1be10c1a06bb
Arifur rahman <arifur.sew@gmail.com>
2026-09-12 12:24:31 +0600

    chore: configure Drizzle ORM and database connection

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "drizzle-kit";
+
+import "dotenv/config";
+
+export default {
+  schema: "./src/core/database/schema/todo.schema.ts",
+  out: "./drizzle",
+
+  dialect: "postgresql",
+
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+} satisfies Config;
